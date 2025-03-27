ae976b11887ac2afc25bb0664f14ac204149c11b
Dmitry Zaguta <zdvbind@gmail.com>
2025-03-27 23:48:38 +0500

    next fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,1 +1,1 @@
-pin_all_from File.expand_path("../app/assets/javascripts", __dir__)
+pin_all_from File.expand_path("../app/assets/javascript", __dir__)
--- test/dummy/app/assets/config/manifest.js
+++ test/dummy/app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
+//= link_directory ../javascript .js
 //= link_directory ../stylesheets .css
 //= link hello_world_manifest.js
 //= link_tree ../../javascript .js
