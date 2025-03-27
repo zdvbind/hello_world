12126f6e0cbb45dd13f4e9d6d88703753150319a
Dmitry Zaguta <zdvbind@gmail.com>
2025-03-27 23:22:45 +0500

    next chapter

--- test/dummy/app/assets/config/manifest.js
+++ test/dummy/app/assets/config/manifest.js
@@ -2,3 +2,5 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 //= link hello_world_manifest.js
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
